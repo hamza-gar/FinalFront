8fc34d2f0ec2d0172adfe0d319970f827e5cd194
hamza-gar <hgarmouche@gmail.com>
2023-02-14 15:39:10 +0100

    voir remarque

--- src/app/components/dashboard/dashboard-etudiant/voir-remarque/voir-remarque.component.ts
+++ src/app/components/dashboard/dashboard-etudiant/voir-remarque/voir-remarque.component.ts
@@ -7,6 +7,8 @@ import {TokenService} from "../../../../services/token.service";
 import {GroupsServiceService} from "../../../../services/groups-service.service";
 import {Router} from "@angular/router";
 import {HttpErrorResponse} from "@angular/common/http";
+import {RemarqueResponse} from "../../../../classes/RemarqueResponse";
+import {EtudiantService} from "../../../../services/etudiant.service";
 
 @Component({
   selector: 'app-voir-remarque',
@@ -14,142 +16,23 @@ import {HttpErrorResponse} from "@angular/common/http";
   styleUrls: ['./voir-remarque.component.css']
 })
 export class VoirRemarqueComponent implements OnInit {
-  public subjects: sujetRequirement = new sujetRequirement();
-  public lisSubjects!:sujetRequirement[];
-  public fullEquipe!:EquipeRequirement[];
 
-  public  member!:Members[][];
-
-  nomSujet!:string;
-  descriptionSujet!:string;
-  tailleEquipe!:number;
-  public p: number = 0;
-
-  public pages!: number[];
-
-  public counter: number = 0;
+  remarqueResponse!:RemarqueResponse[];
 
   constructor(private sujetService: SujetService ,
               private tokenService:TokenService,
               private groupService:GroupsServiceService,
+              private etudiantService:EtudiantService,
               private router:Router) {}
-  public selectedGroup!:EquipeRequirement;
-  public selectedItem!:sujetRequirement ;
 
-  myIndex(index:number){
-    this.selectedItem = this.lisSubjects[index];
-    console.log(this.selectedItem.idSujet);
-    this.getFullEquipes();
-  }
-
-  public addSujet(){
-    this.subjects.nomSujet=this.nomSujet;
-    this.subjects.descriptionSujet=this.descriptionSujet;
-    this.subjects.tailleEquipe=this.tailleEquipe
-    this.sujetService.addSujet(this.subjects).subscribe(operation=>{
-      window.location.reload();
-    }, error=>{
-      console.log('error')
-    })
-
-  }
-
-  public getMySujets(p:number){
-    this.p=p;
-    this.sujetService.getMeSujet(this.p,6).subscribe((response:sujetRequirement[])=>{
-      this.lisSubjects = response;
-      console.log("id :",response);
-    },error => {
-      alert(error)
-    })
+  public getMyRemarque(){
+    this.etudiantService.voiRemarque().subscribe((res:RemarqueResponse[])=>{
+      this.remarqueResponse=res
+    },error=>{(console.log(error))})
   }
   ngOnInit(): void {
-    if (this.p == undefined) {
-      this.p = 0;
-    }
-    this.nomSujet = '';
-    this.descriptionSujet = '';
-    this.tailleEquipe = 0;
-    this.getCount();
-    this.getMySujets(this.p);
-    this.getSujetPages();
-    this.getFullEquipes();
-
-    this.nomSujet=''
-  }
-  public getSujetPages(): void {
-    this.sujetService.getMySujetPages().subscribe(
-      (response: number) => {
-        this.pages = Array.from(Array(Math.ceil(response / 6)).keys());
-        console.log(this.pages)
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  public getCount(): void {
-    this.sujetService.getMySujetPages().subscribe(
-      (response: number) => {
-        this.counter = response;
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
+    this.getMyRemarque()
 
   }
 
-  changePage(page: number) {
-    this.getCount();
-    console.log(Math.ceil( this.counter/ 6))
-    if (page > 0) {
-      this.p++;
-    } else {
-      this.p--;
-    }
-    if(this.p >= 0 && this.p <= (Math.ceil( this.counter/ 6)-1)){
-      this.getMySujets(this.p);
-    }else{
-      if(this.p < 0){
-        this.p = 0;
-      }else{
-        this.p = Math.ceil( this.counter/ 6) - 1;
-      }
-    }
-  }
-
-  public getFullEquipes(){
-    this.groupService.getAllFullEquipes(this.selectedItem.idSujet,this.p,6).subscribe(
-      (response:EquipeRequirement[])=>{
-        this.fullEquipe=response;
-        console.log('hello :',this.fullEquipe)
-      }, error => {console.log(error);});
-  }
-
-  getMemebersOfEquipe(){
-    console.log("this equipe id :" ,this.selectedGroup.idEquipe)
-    this.groupService.getMembersOfEquipe(this.selectedGroup.idEquipe,this.member).subscribe(
-      (operation:Members[][])=>{
-        this.member=operation
-      },error => {
-        console.log(error);
-      })
-  }
-
-  getEquipeIndex(index:any){
-    this.selectedGroup = this.fullEquipe[index]
-    console.log(this.selectedGroup.idEquipe);
-    console.log(this.fullEquipe[index].idEquipe);
-  }
-
-  lockSujet(){
-    this.sujetService.lockSujet(0,this.selectedItem.idSujet,this.selectedGroup.idEquipe).subscribe(respone=>{
-
-      console.log(respone);
-    },error=>{
-      console.log("error");
-    })
-  }
 }
