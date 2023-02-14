c8ff123e0dc335259dd75ad734c6231e02100ef5
Lakh-abdellah <abdellah.lakhssassi@hotmail.com>
2023-02-14 20:04:34 +0100

    chi 3jeb

---
+++ src/app/components/dashboard/dashboard-etudiant/mes-postulation/mes-postulation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-postulation',
+  templateUrl: './mes-postulation.component.html',
+  styleUrls: ['./mes-postulation.component.css']
+})
+export class MesPostulationComponent {
+
+}
