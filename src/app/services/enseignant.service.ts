b0b2d1cfdc9c61a79e1acf23f83682298db523b4
hamza-gar <hgarmouche@gmail.com>
2023-02-15 16:47:26 +0100

    rendes vous

--- src/app/services/enseignant.service.ts
+++ src/app/services/enseignant.service.ts
@@ -30,7 +30,6 @@ export class EnseignantService {
   }
 
 
-
   public getAllEnseignantToInvite(page: number, limit: number):Observable<enseignantSignUp[]>{
     return this.httpClient.get<enseignantSignUp[]>(`http://localhost:8080/jurys/jurys?page=${page}&limit=${limit}`);
   }
